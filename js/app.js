02b9ea0ee26cbac0d039e80bb2d8fa52ea1997b1
Blair <blair.harper@gmail.com>
2018-06-27 11:37:41 +0100

    Added ISS apis

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 // by various parts of application.
 var map;
 var infoWindow;
+var issLat;
+var issLng;
+
 
 // Model
 var pointsOfInterest = [
@@ -10,37 +13,37 @@ var pointsOfInterest = [
         name: "Uddingston",
         address: "G71 7FN",
         lat: 55.818376,
-        long: -4.075098
+        lng: -4.075098
     },
 
     {
         name: "Wembley",
         address: "HA9 6DB",
         lat: 51.553591,
-        long: -0.288527
+        lng: -0.288527
     },
 
     {
         name: "Taunton",
             address: "TA2 7SY",
         lat: 51.030710,
-        long: -3.109492
+        lng: -3.109492
     },
 
     {
         name: "Westminster",
         address: "SW1A 2AA",
         lat: 51.503540,
-        long: -0.127695
+        lng: -0.127695
     },
 
     {
         name: "Newcastle",
         address: "NE1 5DL",
         lat: 54.968291,
-        long: -1.616890
+        lng: -1.616890
 
-    }
+    },
 
 ]
 
@@ -49,19 +52,13 @@ var Place = function(data) {
     this.name = ko.observable(data.name);
     this.address = ko.observable(data.address);
     this.lat = data.lat;
-    this.long = data.long;
-    this.LatLng = {lat: data.lat, lng: data.long}
-    this.ISSdata = ko.observable();
-    /* Need to get this working in separate function */
-    var request = "http://api.open-notify.org/iss-pass.json?lat=" + this.lat + "&lon=" + this.long + "&callback=?";
-    var result;
-    console.log(request);
-     $.getJSON(request).done(function(data) {
-        result = data.response[0].risetime;
-        self.ISSdata(new Date(result*1000));
-     });
-     /* end function */
+    this.lng = data.lng;
+    this.LatLng = {lat: data.lat, lng: data.lng}
 
+    // Get next ISS passing from open-notify API
+    this.ISSdata = ko.observable(getISSdata(self));
+
+    // Place marker on map at this location
     this.marker = new google.maps.Marker({
         position: this.LatLng,
         title: this.name(),
@@ -69,27 +66,45 @@ var Place = function(data) {
     });
     this.marker.setMap(map);
 
-    this.marker.addListener('click', function() {
-    //TODO: add code here for info window
-        animate(self.marker);    
-    });
-
-
 }
 
-// View
+var ISS = function() {
+    var self = this;
+    this.name = "ISS";
+    this.LatLng = ko.observable(getISSloc(self));
+    console.log(this.LatLng());
+    this.marker = new google.maps.Marker({
+        position: this.LatLng,
+        title: this.name,
+        map: map
+    });
+    this.marker.setMap(map);
+}
 
 var viewModel = function() {
     var self = this;
-    
+   
     this.placeList = ko.observableArray([]);
-
+    // Create a new instance of Place for each location
+    // and store in ko observableArray
     pointsOfInterest.forEach(function(placeItem){
         self.placeList.push( new Place(placeItem) );
     });
+    
+    this.placeList.push( new ISS() );
+    this.currentPlace = ko.observable(this.placeList()[2]);
 
-    this.currentPlace = ko.observable(this.placeList()[0]);
+    // Add a listener to each marker that runs the
+    // setPlace function when clicked.
+    this.placeList().forEach(function(place) {
+        place.marker.addListener('click', function() {
+            self.setPlace(place);
+        });
+    
+    });
 
+    // Sets currentPlace to the clicked list item
+    // or clicked marker and animates relevant marker
     this.setPlace = function(clickedPlace) {
         self.currentPlace(clickedPlace);
         animate(self.currentPlace().marker);
@@ -97,7 +112,9 @@ var viewModel = function() {
 
 }
 
+// Initialises map and viewModel bindings
 var viewMap = function() {
+
     this.LatLng = {lat: 51.5074, lng: 0.1278};
     map = new google.maps.Map(document.getElementById('map'), {
         center: this.LatLng,
@@ -105,11 +122,11 @@ var viewMap = function() {
         mapTypeControl: false
     });
 
-ko.applyBindings(new viewModel());
+ ko.applyBindings(new viewModel());
 }
 
+// Make the marker bounce a couple of times
 function animate(marker) {
-    // Make the marker bounce a couple of times when clicked
     marker.setAnimation(google.maps.Animation.BOUNCE);
     setTimeout(function() {
         marker.setAnimation(null);
@@ -118,3 +135,37 @@ function animate(marker) {
     // Center the map position over the position of the clicked marker
     map.panTo(marker.getPosition());
 }
+
+function getISSloc(self) {
+    var api = "http://api.open-notify.org/iss-now.json";
+    var request = api + "?callback=?";
+    var result;
+    $.getJSON(request).done(function(data) {
+        result = {
+                    
+                    lat: data.iss_position.latitude, 
+                    lng: data.iss_position.longitude
+                };
+            
+        self.LatLng(result);
+        console.log(self.LatLng());
+    });
+     
+
+}
+
+// Use open notify API to get ISS crossing info
+// Returns a date: Ddd mmm dd hh:mm:ss timezone
+function getISSdata(self) {
+    var api = "http://api.open-notify.org/iss-pass.json"; 
+    // Need to add a callback to ? so that request is sent JSONP
+    // otherwise browser security features will block the request
+
+    var request = api + "?lat=" + self.lat + "&lon=" + self.lng + "&callback=?";
+    var result;
+     $.getJSON(request).done(function(data) {
+        result = data.response[0].risetime;
+        self.ISSdata(new Date(result*1000));
+     });
+}
+
